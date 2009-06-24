75b91285209ce89f1fb8f6f3b6864dbff6f7706c
Woody Gilk <woody.gilk@kohanaphp.com>
2009-06-24 09:26:07 -0500

    Fixed ending </td> that should have been </th>, fixes #2

--- views/profiler/stats.php
+++ views/profiler/stats.php
@@ -12,7 +12,7 @@
 		</tr>
 		<?php foreach ($benchmarks as $name => $tokens): ?>
 		<tr class="mark time">
-			<th class="name" rowspan="2"><?php echo $name, ' (', count($tokens), ')' ?></td>
+			<th class="name" rowspan="2"><?php echo $name, ' (', count($tokens), ')' ?></th>
 			<?php $stats = Profiler::stats($tokens); ?>
 			<?php foreach (array('min', 'max', 'average', 'total') as $key): ?>
 			<td class="<?php echo $key ?>"><?php echo number_format($stats[$key]['time'], 6), ' ', __('seconds') ?></td>
@@ -29,7 +29,7 @@
 	<table class="profiler">
 		<?php $stats = Profiler::application() ?>
 		<tr class="final mark time">
-			<th class="name" rowspan="2"><?php echo __('Application Execution').' ('.$stats['count'].')' ?></td>
+			<th class="name" rowspan="2"><?php echo __('Application Execution').' ('.$stats['count'].')' ?></th>
 			<?php foreach (array('min', 'max', 'average', 'total') as $key): ?>
 			<td class="<?php echo $key ?>"><?php echo number_format($stats[$key]['time'], 6), ' ', __('seconds') ?></td>
 			<?php endforeach ?>
