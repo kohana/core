50f8f5cd4ca517e386d6a16b9bfba3d59c5b14ac
Woody Gilk <woody.gilk@kohanaphp.com>
2009-06-26 20:00:02 -0500

    Added Fragment class, a view caching helper

---
+++ classes/kohana/fragment.php
@@ -0,0 +1,81 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * View fragment caching.
+ *
+ *     <?php if ( ! Fragment::load('footer')): ?>
+ *     <p>This content will be cached.</p>
+ *     <?php Fragment::save(); endif ?>
+ *
+ * @package    Kohana
+ * @author     Kohana Team
+ * @copyright  (c) 2009 Kohana Team
+ * @license    http://kohanaphp.com/license.html
+ */
+class Kohana_Fragment {
+
+	/**
+	 * @var  integer  number of seconds to cache for
+	 */
+	public static $lifetime = 30;
+
+	// List of buffer => cache key
+	protected static $_caches = array();
+
+	/**
+	 * Load a fragment from cache and display it. Multiple fragments can
+	 * be nested.
+	 *
+	 * @param   string   fragment name
+	 * @return  boolean
+	 */
+	public static function load($name)
+	{
+		// Set the cache key name
+		$cache_key = 'Fragment::cache('.$name.')';
+
+		if ($fragment = Kohana::cache($cache_key, NULL, Fragment::$lifetime))
+		{
+			// Display the cached fragment now
+			echo $fragment;
+
+			return TRUE;
+		}
+		else
+		{
+			// Start the output buffer
+			ob_start();
+
+			// Store the cache key by the buffer level
+			Fragment::$_caches[ob_get_level()] = $cache_key;
+
+			return FALSE;
+		}
+	}
+
+	/**
+	 * Saves a fragment in the cache.
+	 *
+	 * @return  void
+	 */
+	public static function save()
+	{
+		// Get the buffer level
+		$level = ob_get_level();
+
+		if (isset(Fragment::$_caches[$level]))
+		{
+			// Get the cache key based on the level
+			$cache_key = Fragment::$_caches[$level];
+
+			// Delete the cache key, we don't need it anymore
+			unset(Fragment::$_caches[$level]);
+
+			// Get the output buffer and display it at the same time
+			$fragment = ob_get_flush();
+
+			// Cache the fragment
+			Kohana::cache($cache_key, $fragment, Fragment::$lifetime);
+		}
+	}
+
+} // End Fragment
