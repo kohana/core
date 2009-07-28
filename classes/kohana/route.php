96ee0200e90337656115d9c0a5155e72a1bae139
Geert De Deckere <geert@idoe.be>
2009-07-28 09:29:45 +0200

    Closes #74

--- classes/kohana/route.php
+++ classes/kohana/route.php
@@ -257,6 +257,11 @@ class Kohana_Route {
 			// Use the default parameters
 			$params = $this->_defaults;
 		}
+		else
+		{
+			// Merge with default parameters
+			$params += $this->_defaults;
+		}
 
 		// Start with the routed URI
 		$uri = $this->_uri;
