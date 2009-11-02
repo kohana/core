c6122c0c2d00cc7c6e47cbb1b7e678ab2b4dcc38
Woody Gilk <woody.gilk@kohanaphp.com>
2009-11-02 14:41:06 -0600

    Added declaration of KOHANA_START_* to base.php, if not already defined

--- base.php
+++ base.php
@@ -1,5 +1,21 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+if ( ! defined('KOHANA_START_TIME'))
+{
+	/**
+	 * Define the start time of the application, used for profiling.
+	 */
+	define('KOHANA_START_TIME', microtime(TRUE));
+}
+
+if ( ! defined('KOHANA_START_MEMORY'))
+{
+	/**
+	 * Define the memory usage at the start of the application, used for profiling.
+	 */
+	define('KOHANA_START_MEMORY', memory_get_usage());
+}
+
 /**
  * Kohana translation/internationalization function.
  *
