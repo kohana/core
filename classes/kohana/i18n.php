kohana/core

commit e5f3685af265252e81144d16d39a8d271177542d

Make it possible to set I18n::$lang to a value that does not include a region or locale, fixes #2266

classes/kohana/i18n.php
@@ -46,31 +46,30 @@ class Kohana_I18n {
 	{
 		if ( ! isset(I18n::$_cache[$lang]))
 		{
-			// Separate the language and locale
-			list ($language, $locale) = explode('-', strtolower($lang), 2);
-
-			// Start a new translation table
+			// New translation table
 			$table = array();
 
-			// Add the non-specific language strings
-			if ($files = Kohana::find_file('i18n', $language))
-			{
-				foreach ($files as $file)
-				{
-					// Merge the language strings into the translation table
-					$table = array_merge($table, require $file);
-				}
-			}
+			// Split the language: language, region, locale, etc
+			$parts = explode('-', $lang);
 
-			// Add the locale-specific language strings
-			if ($files = Kohana::find_file('i18n', $language.DIRECTORY_SEPARATOR.$locale))
+			do
 			{
-				foreach ($files as $file)
+				// Create a path from the parts left
+				$path = implode(DIRECTORY_SEPARATOR, $parts);
+
+				if ($files = Kohana::find_file('i18n', $path))
 				{
-					// Merge the locale strings into the translation table
-					$table = array_merge($table, require $file);
+					foreach ($files as $file)
+					{
+						// Merge the language strings into the translation table
+						$table = array_merge($table, Kohana::load($file));
+					}
 				}
+
+				// Remove the last part
+				array_pop($parts);
 			}
+			while ($parts);
 
 			// Cache the translation table locally
 			I18n::$_cache[$lang] = $table;

commit b392b7cdb16c80fddcba0e47103d07afe2cbf484

Fixes a bug introduced in e5f3685af265252e81144d16d39a8d271177542d that would prevent region and locale translations from overloading the language table

classes/kohana/i18n.php
@@ -76,11 +76,18 @@ class Kohana_I18n {
 
 				if ($files = Kohana::find_file('i18n', $path))
 				{
+					// Translation sub table
+					$t = array();
+
 					foreach ($files as $file)
 					{
-						// Merge the language strings into the translation table
-						$table = array_merge($table, Kohana::load($file));
+						// Merge the language strings into the sub table
+						$t = array_merge($table, Kohana::load($file));
 					}
+
+					// Append the sub table, preventing less specific language
+					// files from overloading more specific files
+					$table += $t;
 				}
 
 				// Remove the last part