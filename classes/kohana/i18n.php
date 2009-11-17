6b8ae47099a92addb41d74e3173057738f659b60
Woody Gilk <woody.gilk@kohanaphp.com>
2009-11-17 00:20:02 -0600

    Cleaning up I18n::load()

--- classes/kohana/i18n.php
+++ classes/kohana/i18n.php
@@ -61,45 +61,44 @@ class Kohana_I18n {
 	 */
 	public static function load($lang)
 	{
-		if ( ! isset(I18n::$_cache[$lang]))
+		if (isset(I18n::$_cache[$lang]))
 		{
-			// New translation table
-			$table = array();
-
-			// Split the language: language, region, locale, etc
-			$parts = explode('-', $lang);
-
-			do
-			{
-				// Create a path from the parts left
-				$path = implode(DIRECTORY_SEPARATOR, $parts);
-
-				if ($files = Kohana::find_file('i18n', $path))
-				{
-					// Translation sub table
-					$t = array();
-
-					foreach ($files as $file)
-					{
-						// Merge the language strings into the sub table
-						$t = array_merge($table, Kohana::load($file));
-					}
-
-					// Append the sub table, preventing less specific language
-					// files from overloading more specific files
-					$table += $t;
-				}
-
-				// Remove the last part
-				array_pop($parts);
-			}
-			while ($parts);
-
-			// Cache the translation table locally
-			I18n::$_cache[$lang] = $table;
+			return I18n::$_cache[$lang];
 		}
 
-		return I18n::$_cache[$lang];
+		// New translation table
+		$table = array();
+
+		// Split the language: language, region, locale, etc
+		$parts = explode('-', $lang);
+
+		do
+		{
+			// Create a path for this set of parts
+			$path = implode(DIRECTORY_SEPARATOR, $parts);
+
+			if ($files = Kohana::find_file('i18n', $path))
+			{
+				$t = array();
+
+				foreach ($files as $file)
+				{
+					// Merge the language strings into the sub table
+					$t = array_merge($table, Kohana::load($file));
+				}
+
+				// Append the sub table, preventing less specific language
+				// files from overloading more specific files
+				$table += $t;
+			}
+
+			// Remove the last part
+			array_pop($parts);
+		}
+		while ($parts);
+
+		// Cache the translation table locally
+		return I18n::$_cache[$lang] = $table;
 	}
 
 	final private function __construct()
