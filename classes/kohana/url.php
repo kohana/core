3597a8c0c12b7e96c58231121fd7dd4cb9d4c726
Geert De Deckere <geert@idoe.be>
2009-11-30 21:13:16 +0100

    Updated URL::title to accept any character as word separator.
    This makes the function a bit more flexible. For example, you could use a dot as separator to generate usernames like "geert.de.deckere".

--- classes/kohana/url.php
+++ classes/kohana/url.php
@@ -110,30 +110,28 @@ class Kohana_URL {
 	 * Convert a phrase to a URL-safe title.
 	 *
 	 * @param   string   phrase to convert
-	 * @param   string   word separator (- or _)
+	 * @param   string   word separator (any single character)
 	 * @param   boolean  transliterate to ASCII
 	 * @return  string
 	 */
 	public static function title($title, $separator = '-', $ascii_only = FALSE)
 	{
-		$separator = ($separator === '-') ? '-' : '_';
-
 		if ($ascii_only === TRUE)
 		{
 			// Transliterate non-ASCII characters
 			$title = UTF8::transliterate_to_ascii($title);
 
 			// Remove all characters that are not the separator, a-z, 0-9, or whitespace
-			$title = preg_replace('/[^'.$separator.'a-z0-9\s]+/', '', strtolower($title));
+			$title = preg_replace('![^'.preg_quote($separator).'a-z0-9\s]+!', '', strtolower($title));
 		}
 		else
 		{
 			// Remove all characters that are not the separator, letters, numbers, or whitespace
-			$title = preg_replace('/[^'.$separator.'\pL\pN\s]+/u', '', UTF8::strtolower($title));
+			$title = preg_replace('![^'.preg_quote($separator).'\pL\pN\s]+!u', '', UTF8::strtolower($title));
 		}
 
 		// Replace all separator characters and whitespace by a single separator
-		$title = preg_replace('/['.$separator.'\s]+/', $separator, $title);
+		$title = preg_replace('!['.preg_quote($separator).'\s]+!u', $separator, $title);
 
 		// Trim separators from the beginning and end
 		return trim($title, $separator);
