8e1a1bb4dd074996ff7e170df75d5bca4fd66a00
Woody Gilk <woody.gilk@kohanaphp.com>
2010-04-04 07:26:56 +0200

    Follow up to e5b7111865680699fb201b9120e3f6e8efc51916, added Kohana::$shutdown_errors array with a list of all error types that will be handled in Kohana::shutdown_handler(). This allows users to decide which errors should be handled.

--- classes/kohana/core.php
+++ classes/kohana/core.php
@@ -110,6 +110,11 @@ class Kohana_Core {
 	public static $errors = TRUE;
 
 	/**
+	 * @var  array  types of errors to display at shutdown
+	 */
+	public static $shutdown_errors = array(E_PARSE, E_ERROR, E_USER_ERROR, E_COMPILE_ERROR);
+
+	/**
 	 * @var  object  logging object
 	 */
 	public static $log;
@@ -990,7 +995,7 @@ class Kohana_Core {
 			Kohana::exception_handler($e);
 		}
 
-		if (Kohana::$errors AND $error = error_get_last() AND $error['type'] === E_ERROR)
+		if (Kohana::$errors AND $error = error_get_last() AND in_array($error['type'], Kohana::$shutdown_errors))
 		{
 			// Clean the output buffer
 			ob_get_level() and ob_clean();
