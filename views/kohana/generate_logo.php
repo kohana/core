de2ab297081b44ab6c6ca9cf606c8b06d40bee3e
Woody Gilk <woody.gilk@kohanaphp.com>
2010-05-08 14:09:15 +0200

    Updated the generate_logo.php view and logo.php with the current v3 logo

--- views/kohana/generate_logo.php
+++ views/kohana/generate_logo.php
@@ -1,14 +1,14 @@
 <?php
 
 // Get the latest logo contents
-$data = base64_encode(file_get_contents('http://kohanaphp.com/3.0/media/img/kohana.png'));
+$data = base64_encode(file_get_contents('http://kohanaframework.org/media/img/kohana.png'));
 
 // Create the logo file
 file_put_contents('logo.php', "<?php
 /**
  * Kohana Logo, base64_encoded PNG
  * 
- * @copyright  (c) 2008-2009 Kohana Team
+ * @copyright  (c) 2008-2010 Kohana Team
  * @license    http://kohanaphp.com/license.html
  */
 return array('mime' => 'image/png', 'data' => '{$data}'); ?>");
