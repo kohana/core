9cbe795ba0adeb20b3dc0ffd063a86325677120f
Woody Gilk <woody.gilk@kohanaphp.com>
2010-07-03 07:50:14 -0500

    Reapply b7aa13dac330add85883692b2d063882169f1b0e

--- views/kohana/error.php
+++ views/kohana/error.php
@@ -48,7 +48,7 @@ function koggle(elem)
 }
 </script>
 <div id="kohana_error">
-	<h1><span class="type"><?php echo $type ?> [ <?php echo $code ?> ]:</span> <span class="message"><?php echo $message ?></span></h1>
+	<h1><span class="type"><?php echo $type ?> [ <?php echo $code ?> ]:</span> <span class="message"><?php echo html::chars($message) ?></span></h1>
 	<div id="<?php echo $error_id ?>" class="content">
 		<p><span class="file"><?php echo Kohana::debug_path($file) ?> [ <?php echo $line ?> ]</span></p>
 		<?php echo Kohana::debug_source($file, $line) ?>
