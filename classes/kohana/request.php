543222980342b5262a1e97062fdee954166f13d1
Lorenzo Pisani <zeelot3k@gmail.com>
2010-07-21 01:44:51 +0800

    making sure all methods of a controller exist before executing any of them. fixes #3099

--- classes/kohana/request.php
+++ classes/kohana/request.php
@@ -1036,17 +1036,22 @@ class Kohana_Request {
 			// Create a new instance of the controller
 			$controller = $class->newInstance($this);
 
-			// Execute the "before action" method
-			$class->getMethod('before')->invoke($controller);
-
 			// Determine the action to use
 			$action = empty($this->action) ? Route::$default_action : $this->action;
 
+			// Get all the method objects before invoking them
+			$before = $class->getMethod('before');
+			$method = $class->getMethod('action_'.$action);
+			$after = $class->getMethod('after');
+
+			// Execute the "before action" method
+			$before->invoke($controller);
+
 			// Execute the main action with the parameters
-			$class->getMethod('action_'.$action)->invokeArgs($controller, $this->_params);
+			$method->invokeArgs($controller, $this->_params);
 
 			// Execute the "after action" method
-			$class->getMethod('after')->invoke($controller);
+			$after->invoke($controller);
 		}
 		catch (Exception $e)
 		{
