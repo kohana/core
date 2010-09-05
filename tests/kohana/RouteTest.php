e6aedf00d566e224c48cb062b33b5b69479a475d
BRMatt <matthew@sigswitch.com>
2010-09-05 13:03:40 +0100

    Changing route test to use unittest helper

--- tests/kohana/RouteTest.php
+++ tests/kohana/RouteTest.php
@@ -20,7 +20,7 @@ class Kohana_RouteTest extends Kohana_Unittest_TestCase
 	{
 		parent::setUp();
 
-		$this->clean_cache_dir();
+		$this->cleanCacheDir();
 	}
 
 	/**
@@ -30,38 +30,7 @@ class Kohana_RouteTest extends Kohana_Unittest_TestCase
 	{
 		parent::tearDown();
 
-		$this->clean_cache_dir();
-	}
-
-	/**
-	 * Removes all kohana related cache files in the cache directory
-	 */
-	public function clean_cache_dir()
-	{
-		$cache_dir = opendir(Kohana::$cache_dir);
-
-		while($dir = readdir($cache_dir))
-		{
-			// Cache files are split into directories based on first two characters of hash
-			if($dir[0] !== '.' AND strlen($dir) === 2)
-			{
-				$cache = opendir(Kohana::$cache_dir.'/'.$dir);
-
-				while($file = readdir($cache))
-				{
-					if($file[0] !== '.')
-					{
-						unlink(Kohana::$cache_dir.'/'.$dir.'/'.$file);
-					}
-				}
-
-				closedir($cache);
-
-				rmdir(Kohana::$cache_dir.'/'.$dir);
-			}
-		}
-
-		closedir($cache_dir);
+		$this->cleanCacheDir();
 	}
 
 	/**
@@ -140,7 +109,7 @@ class Kohana_RouteTest extends Kohana_Unittest_TestCase
 	 * Route::cache() should return FALSE if cached routes could not be found
 	 *
 	 * The cache is cleared before and after each test in setUp tearDown 
-	 * by clean_cache_dir()
+	 * by cleanCacheDir()
 	 * 
 	 * @test
 	 * @covers Route::cache
