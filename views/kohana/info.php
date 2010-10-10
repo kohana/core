9b43897efd04c4c6198ec5a573df6a5bdec65105
bluehawk <phroughy@gmail.com>
2010-10-10 01:53:28 -0600

    Added the tests from install.php

--- views/kohana/info.php
+++ views/kohana/info.php
@@ -124,5 +124,159 @@
 	<p>No routes</p>
 	<?php endif; ?>
 	
+	<h2>install.php tests</h2>
+	
+	<table cellspacing="0">
+		<tr>
+			<th>PHP Version</th>
+			<?php if (version_compare(PHP_VERSION, '5.2.3', '>=')): ?>
+				<td class="pass"><?php echo PHP_VERSION ?></td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">Kohana requires PHP 5.2.3 or newer, this version is <?php echo PHP_VERSION ?>.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>System Directory</th>
+			<?php if (is_dir(SYSPATH) AND is_file(SYSPATH.'classes/kohana'.EXT)): ?>
+				<td class="pass"><?php echo SYSPATH ?></td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">The configured <code>system</code> directory does not exist or does not contain required files.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>Application Directory</th>
+			<?php if (is_dir(APPPATH) AND is_file(APPPATH.'bootstrap'.EXT)): ?>
+				<td class="pass"><?php echo APPPATH ?></td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">The configured <code>application</code> directory does not exist or does not contain required files.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>Cache Directory</th>
+			<?php if (is_dir(APPPATH) AND is_dir(APPPATH.'cache') AND is_writable(APPPATH.'cache')): ?>
+				<td class="pass"><?php echo APPPATH.'cache/' ?></td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">The <code><?php echo APPPATH.'cache/' ?></code> directory is not writable.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>Logs Directory</th>
+			<?php if (is_dir(APPPATH) AND is_dir(APPPATH.'logs') AND is_writable(APPPATH.'logs')): ?>
+				<td class="pass"><?php echo APPPATH.'logs/' ?></td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">The <code><?php echo APPPATH.'logs/' ?></code> directory is not writable.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>PCRE UTF-8</th>
+			<?php if ( ! @preg_match('/^.$/u', 'ñ')): $failed = TRUE ?>
+				<td class="fail"><a href="http://php.net/pcre">PCRE</a> has not been compiled with UTF-8 support.</td>
+			<?php elseif ( ! @preg_match('/^\pL$/u', 'ñ')): $failed = TRUE ?>
+				<td class="fail"><a href="http://php.net/pcre">PCRE</a> has not been compiled with Unicode property support.</td>
+			<?php else: ?>
+				<td class="pass">Pass</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>SPL Enabled</th>
+			<?php if (function_exists('spl_autoload_register')): ?>
+				<td class="pass">Pass</td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">PHP <a href="http://www.php.net/spl">SPL</a> is either not loaded or not compiled in.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>Reflection Enabled</th>
+			<?php if (class_exists('ReflectionClass')): ?>
+				<td class="pass">Pass</td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">PHP <a href="http://www.php.net/reflection">reflection</a> is either not loaded or not compiled in.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>Filters Enabled</th>
+			<?php if (function_exists('filter_list')): ?>
+				<td class="pass">Pass</td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">The <a href="http://www.php.net/filter">filter</a> extension is either not loaded or not compiled in.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>Iconv Extension Loaded</th>
+			<?php if (extension_loaded('iconv')): ?>
+				<td class="pass">Pass</td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">The <a href="http://php.net/iconv">iconv</a> extension is not loaded.</td>
+			<?php endif ?>
+		</tr>
+		<?php if (extension_loaded('mbstring')): ?>
+		<tr>
+			<th>Mbstring Not Overloaded</th>
+			<?php if (ini_get('mbstring.func_overload') & MB_OVERLOAD_STRING): $failed = TRUE ?>
+				<td class="fail">The <a href="http://php.net/mbstring">mbstring</a> extension is overloading PHP's native string functions.</td>
+			<?php else: ?>
+				<td class="pass">Pass</td>
+			<?php endif ?>
+		</tr>
+		<?php endif ?>
+		<tr>
+			<th>Character Type (CTYPE) Extension</th>
+			<?php if ( ! function_exists('ctype_digit')): $failed = TRUE ?>
+				<td class="fail">The <a href="http://php.net/ctype">ctype</a> extension is not enabled.</td>
+			<?php else: ?>
+				<td class="pass">Pass</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>URI Determination</th>
+			<?php if (isset($_SERVER['REQUEST_URI']) OR isset($_SERVER['PHP_SELF']) OR isset($_SERVER['PATH_INFO'])): ?>
+				<td class="pass">Pass</td>
+			<?php else: $failed = TRUE ?>
+				<td class="fail">Neither <code>$_SERVER['REQUEST_URI']</code>, <code>$_SERVER['PHP_SELF']</code>, or <code>$_SERVER['PATH_INFO']</code> is available.</td>
+			<?php endif ?>
+		</tr>
+	</table>
+	
+	<h3>Optional Tests</h3>
+
+	<p>
+		The following extensions are not required to run the Kohana core, but if enabled can provide access to additional classes.
+	</p>
+
+	<table cellspacing="0">
+		<tr>
+			<th>cURL Enabled</th>
+			<?php if (extension_loaded('curl')): ?>
+				<td class="pass">Pass</td>
+			<?php else: ?>
+				<td class="fail">Kohana requires <a href="http://php.net/curl">cURL</a> for the Remote class.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>mcrypt Enabled</th>
+			<?php if (extension_loaded('mcrypt')): ?>
+				<td class="pass">Pass</td>
+			<?php else: ?>
+				<td class="fail">Kohana requires <a href="http://php.net/mcrypt">mcrypt</a> for the Encrypt class.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>GD Enabled</th>
+			<?php if (function_exists('gd_info')): ?>
+				<td class="pass">Pass</td>
+			<?php else: ?>
+				<td class="fail">Kohana requires <a href="http://php.net/gd">GD</a> v2 for the Image class.</td>
+			<?php endif ?>
+		</tr>
+		<tr>
+			<th>PDO Enabled</th>
+			<?php if (class_exists('PDO')): ?>
+				<td class="pass">Pass</td>
+			<?php else: ?>
+				<td class="fail">Kohana can use <a href="http://php.net/pdo">PDO</a> to support additional databases.</td>
+			<?php endif ?>
+		</tr>
+	</table>
+	
 </body>
 </html>
