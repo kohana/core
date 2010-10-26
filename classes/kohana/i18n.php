3d5c80b0549562e97aeeed7638131a8de620d0b2
Jeremy Bush <contractfrombelow@gmail.com>
2010-10-26 18:57:33 -0500

    Move __() to i18n class file. Partialy fixes #3249.

--- classes/kohana/i18n.php
+++ classes/kohana/i18n.php
@@ -130,3 +130,29 @@ class Kohana_I18n {
 	}
 
 } // End I18n
+
+/**
+ * Kohana translation/internationalization function. The PHP function
+ * [strtr](http://php.net/strtr) is used for replacing parameters.
+ *
+ *    __('Welcome back, :user', array(':user' => $username));
+ *
+ * [!!] The target language is defined by [I18n::$lang].
+ * 
+ * @uses    I18n::get
+ * @param   string  text to translate
+ * @param   array   values to replace in the translated text
+ * @param   string  source language
+ * @return  string
+ */
+function __($string, array $values = NULL, $lang = 'en-us')
+{
+	if ($lang !== I18n::$lang)
+	{
+		// The message and target languages are different
+		// Get the translation for this message
+		$string = I18n::get($string);
+	}
+
+	return empty($values) ? $string : strtr($string, $values);
+}
