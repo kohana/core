156d55dd00d7e5143c3b2eee02b67ce42cd6d161
Geert De Deckere <geert@idoe.be>
2010-10-30 07:26:54 +0200

    Revert "Cleaning up URL::query, no need to check if the query is empty, we already checked the parameters"
    This reverts commit b495107eb74bd244bcac7899b3c09075a279ab4f, related to #2918.

--- classes/kohana/url.php
+++ classes/kohana/url.php
@@ -142,8 +142,11 @@ class Kohana_URL {
 			return '';
 		}
 
-		// ? is added for simple string concatenation
-		return '?'.http_build_query($params, '', '&');
+		// Note: http_build_query returns an empty string for a params array with only NULL values
+		$query = http_build_query($params, '', '&');
+
+		// Don't prepend '?' to an empty string
+		return ($query === '') ? '' : '?'.$query;
 	}
 
 	/**
