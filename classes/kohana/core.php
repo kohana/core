584bdca533a593cbec45a9c2eabfb3ae56c36acb
Sam de Freyssinet <sam@def.reyssi.net>
2010-10-31 01:41:39 +0100

    Added Kohana::$hostnames to core for registry of localhosts

--- classes/kohana/core.php
+++ classes/kohana/core.php
@@ -81,6 +81,11 @@ class Kohana_Core {
 	public static $charset = 'utf-8';
 
 	/**
+	 * @var  string  the name of the server Kohana is hosted upon
+	 */
+	public static $server_name = '';
+
+	/**
 	 * @var  string  base URL to the application
 	 */
 	public static $base_url = '/';
