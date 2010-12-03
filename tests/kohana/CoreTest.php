6ac7478a181d565cf92cde029670dcbc1d96c0a4
Mathew Davies <thepixeldeveloper@googlemail.com>
2010-12-03 04:44:13 +0000

    Updated the messages test.

--- tests/kohana/CoreTest.php
+++ tests/kohana/CoreTest.php
@@ -155,7 +155,7 @@ class Kohana_CoreTest extends Kohana_Unittest_TestCase
 			array(
 				array(
 					'alpha'         => ':field must contain only letters',
-					'alpha_dash'    => ':field must contain only letters and dashes',
+					'alpha_dash'    => ':field must contain only numbers, letters and dashes',
 					'alpha_numeric' => ':field must contain only letters and numbers',
 					'color'         => ':field must be a color',
 					'credit_card'   => ':field must be a credit card number',
@@ -164,9 +164,11 @@ class Kohana_CoreTest extends Kohana_Unittest_TestCase
 					'digit'         => ':field must be a digit',
 					'email'         => ':field must be a email address',
 					'email_domain'  => ':field must contain a valid email domain',
+					'equals'        => ':field must equal :param1',
 					'exact_length'  => ':field must be exactly :param1 characters long',
 					'in_array'      => ':field must be one of the available options',
 					'ip'            => ':field must be an ip address',
+					'numeric'       => ':field must be numeric',
 					'matches'       => ':field must be the same as :param1',
 					'min_length'    => ':field must be at least :param1 characters long',
 					'max_length'    => ':field must be less than :param1 characters long',
