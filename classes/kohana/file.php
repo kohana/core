0c1e4b43058b70f57ef93472b81a691625ada717
Kiall Mac Innes <kiall@managedit.ie>
2010-12-06 03:10:19 +0000

    Cleanup cs278's commits for coding style and fix a bug preventing transparant extension. refs #3167

--- classes/kohana/file.php
+++ classes/kohana/file.php
@@ -96,9 +96,8 @@ class Kohana_File {
 	/**
 	 * Lookup file extensions by MIME type
 	 * 
-	 * @staticvar array $types Cache of MIME to extension mappings
-	 * @param string $type File MIME type
-	 * @return array File extensions matching MIME type
+	 * @param   string  $type File MIME type
+	 * @return  array   File extensions matching MIME type
 	 */
 	public static function exts_by_mime($type)
 	{
@@ -117,11 +116,11 @@ class Kohana_File {
 						continue;
 					}
 
-					if (!isset($types[$mime]))
+					if ( ! isset($types[$mime]))
 					{
 						$types[$mime] = array((string) $ext);
 					}
-					else if (!in_array($ext, $types[$mime]))
+					else if ( ! in_array($ext, $types[$mime]))
 					{
 						$types[$mime][] = (string) $ext;
 					}
@@ -129,19 +128,18 @@ class Kohana_File {
 			}
 		}
 
-		return isset($types[$type]) ? $types[$type] : false;
+		return isset($types[$type]) ? $types[$type] : FALSE;
 	}
 
 	/**
 	 * Lookup a single file extension by MIME type.
 	 * 
-	 * @uses File::exts_by_mime()
-	 * @param string $type MIME type to lookup
-	 * @return mixed First file extension matching or false
+	 * @param   string  $type  MIME type to lookup
+	 * @return  mixed          First file extension matching or false
 	 */
 	public static function ext_by_mime($type)
 	{
-		return current(self::exts_by_mime($type));
+		return current(File::exts_by_mime($type));
 	}
 
 	/**
