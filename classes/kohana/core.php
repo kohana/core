37d6d2d7e9ddbbf6adb8c57c7a31cf999d6e2993
Isaiah DeRose-Wilson <isaiah@iitechs.com>
2010-12-08 20:56:22 -0500

    Removed E_COMPILE_ERROR from Kohana::$shutdown_errors, compile errors cause the wrong error message to be shown most of the time. Fixes #3467

--- classes/kohana/core.php
+++ classes/kohana/core.php
@@ -114,7 +114,7 @@ class Kohana_Core {
 	/**
 	 * @var  array  types of errors to display at shutdown
 	 */
-	public static $shutdown_errors = array(E_PARSE, E_ERROR, E_USER_ERROR, E_COMPILE_ERROR);
+	public static $shutdown_errors = array(E_PARSE, E_ERROR, E_USER_ERROR);
 
 	/**
 	 * @var  boolean  set the X-Powered-By header
