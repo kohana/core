2198b5501cbff7b56b5a57bc22ad1d59ea9ab126
Jeremy Bush <contractfrombelow@gmail.com>
2011-02-18 11:22:32 -0600

    Store different caches for $array vs non $array for Kohana::find_file(), Fixes #3674

--- classes/kohana/core.php
+++ classes/kohana/core.php
@@ -627,10 +627,10 @@ class Kohana_Core {
 		// Create a partial path of the filename
 		$path = $dir.DIRECTORY_SEPARATOR.$file.$ext;
 
-		if (Kohana::$caching === TRUE AND isset(Kohana::$_files[$path]))
+		if (Kohana::$caching === TRUE AND isset(Kohana::$_files[$path.($array ? '_array' : '_path')]))
 		{
 			// This path has been cached
-			return Kohana::$_files[$path];
+			return Kohana::$_files[$path.($array ? '_array' : '_path')];
 		}
 
 		if (Kohana::$profiling === TRUE AND class_exists('Profiler', FALSE))
@@ -677,7 +677,7 @@ class Kohana_Core {
 		if (Kohana::$caching === TRUE)
 		{
 			// Add the path to the cache
-			Kohana::$_files[$path] = $found;
+			Kohana::$_files[$path.($array ? '_array' : '_path')] = $found;
 
 			// Files have been changed
 			Kohana::$_files_changed = TRUE;
