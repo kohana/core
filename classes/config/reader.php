9bb898aba2f8d8f5261f36c5dee4e18a1361e3e2
Matt Button <matthew@sigswitch.com>
2011-03-29 13:22:23 +0100

    Adding transparent extensions for the reader/writer config interfaces

--- classes/config/reader.php
+++ classes/config/reader.php
@@ -1,3 +1,15 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Config_Reader extends Kohana_Config_Reader {}
+/**
+ * Transparent extension of the Kohana_Config_Reader interface
+ *
+ * @package    Kohana
+ * @category   Configuration
+ * @author     Kohana Team
+ * @copyright  (c) 2007-2011 Kohana Team
+ * @license    http://kohanaframework.org/license
+ */
+interface Config_Reader extends Kohana_Config_Reader
+{
+
+}
---
+++ classes/config/writer.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Transparent extension of the Kohana_Config_Writer interface
+ *
+ * @package    Kohana/Database
+ * @category   Configuration
+ * @author     Kohana Team
+ * @copyright  (c) 2011 Kohana Team
+ * @license    http://kohanaframework.org/license
+ */
+interface Config_Writer extends Kohana_Config_Writer
+{
+
+}
