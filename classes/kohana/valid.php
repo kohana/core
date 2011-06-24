a4d2c4410ee9f79128bb60cd873d3d77872850cd
Lorenzo Pisani <zeelot3k@gmail.com>
2011-06-23 21:51:20 -0700

    Valid::email_domain() doesn't need to run checkdnsrr() on empty fields (refs #3552)

--- classes/kohana/valid.php
+++ classes/kohana/valid.php
@@ -144,6 +144,9 @@ class Kohana_Valid {
 	 */
 	public static function email_domain($email)
 	{
+		if ( ! Valid::not_empty($email))
+			return FALSE; // Empty fields cause issues with checkdnsrr()
+
 		// Check if the email domain has a valid MX record
 		return (bool) checkdnsrr(preg_replace('/^[^@]++@/', '', $email), 'MX');
 	}
