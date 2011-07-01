f39a85a5444e5d8fa24550c0ce6e47169919d93e
Sam de Freyssinet <sam@def.reyssi.net>
2011-07-01 10:44:03 +0100

    Fixes #4073, Request_Client_External can now handle options that include FALSE values. Thanks Nick.

--- classes/kohana/request/client/external.php
+++ classes/kohana/request/client/external.php
@@ -191,7 +191,7 @@ abstract class Kohana_Request_Client_External extends Request_Client {
 		{
 			$this->_options = $key;
 		}
-		elseif ( ! $value)
+		elseif ($value === NULL)
 		{
 			return Arr::get($this->_options, $key);
 		}
