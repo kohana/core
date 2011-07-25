kohana/core

commit 035155ef185671deb13cb6d99e1756479cf0c5f2

Fixes #4123. The HTTP referer [sic] header is set to redirects
Additionally, the response body (if present) is output along with any other headers set the response upto this point

classes/kohana/request.php
@@ -942,23 +942,29 @@ class Kohana_Request implements HTTP_Request {
 	 */
 	public function redirect($url = '', $code = 302)
 	{
+		$referrer = $this->uri();
+
+		if (strpos($referrer, '://') === FALSE)
+		{
+			$referrer = URL::site($referrer, TRUE, Kohana::$index_file);
+		}
+
 		if (strpos($url, '://') === FALSE)
 		{
 			// Make the URI into a URL
-			$url = URL::site($url, TRUE);
+			$url = URL::site($url, TRUE, Kohana::$index_file);
 		}
 
-		// Redirect
-		$response = $this->create_response();
+		if (($response = $this->response()) === NULL)
+		{
+			$response = $this->create_response();
+		}
 
-		// Set the response status
-		$response->status($code);
-
-		// Set the location header
-		$response->headers('Location', $url);
-
-		// Send headers
-		$response->send_headers();
+		echo $response->status($code)
+			->headers('Location', $url)
+			->headers('Referer', $referrer)
+			->send_headers()
+			->body();
 
 		// Stop execution
 		exit;

commit cc9394deac09da78ca8a036b959ad945f233711c

035155ef185671deb13c Broke Request::redirect() for setups using an index_page, since URL::base() (via URL::site()) expects the $index argument to be boolean, not the string to use.

classes/kohana/request.php
@@ -946,13 +946,13 @@ class Kohana_Request implements HTTP_Request {
 
 		if (strpos($referrer, '://') === FALSE)
 		{
-			$referrer = URL::site($referrer, TRUE, Kohana::$index_file);
+			$referrer = URL::site($referrer, TRUE, !empty(Kohana::$index_file));
 		}
 
 		if (strpos($url, '://') === FALSE)
 		{
 			// Make the URI into a URL
-			$url = URL::site($url, TRUE, Kohana::$index_file);
+			$url = URL::site($url, TRUE, !empty(Kohana::$index_file));
 		}
 
 		if (($response = $this->response()) === NULL)