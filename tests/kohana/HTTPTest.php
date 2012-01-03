aa68cd6cefceed24411ace99b6cff8ac924353cd
Kiall Mac Innes <kiall@managedit.ie>
2012-01-03 14:59:16 +0000

    Remove tests that no longer apply.

--- tests/kohana/HTTPTest.php
+++ tests/kohana/HTTPTest.php
@@ -14,43 +14,4 @@
  */
 class Kohana_HTTPTest extends Unittest_TestCase {
 
-	/**
-	 * Tests HTTP::redirect() for absolute URIs
-	 *
-	 * @test
-	 */
-	public function test_redirect_absolute()
-	{
-		$request = Request::factory('http://example.com/uri');
-		$response = Response::factory()
-			->headers('X-Test', 'Test')
-			->headers('Referrer', 'ShouldBeReplaced');
-
-		HTTP::redirect($request, $response, 'http://example.org', 303);
-
-		$this->assertSame($response->status(), 303);
-		$this->assertSame($response->headers('Location'), 'http://example.org');
-		$this->assertSame($response->headers('Referrer'), 'http://example.com/uri');
-		$this->assertSame($response->headers('X-Test'), 'Test');
-	}
-
-	/**
-	 * Tests HTTP::redirect() for relative URIs
-	 *
-	 * @test
-	 */
-	public function test_redirect_relative()
-	{
-		$request = Request::factory('https://example.com/uri');
-		$response = Response::factory()
-			->headers('X-Test', 'Test')
-			->headers('Referrer', 'ShouldBeReplaced');
-
-		HTTP::redirect($request, $response, 'account/login', 307);
-
-		$this->assertSame($response->status(), 307);
-		$this->assertSame($response->headers('Location'), 'http://example.org');
-		$this->assertSame($response->headers('Referrer'), 'https://example.com/uri');
-		$this->assertSame($response->headers('X-Test'), 'Test');
-	}
 }
