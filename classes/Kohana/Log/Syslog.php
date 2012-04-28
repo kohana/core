1ab4df33603cb0a4bc911d0820ef7fecd894fc8b
Mathew Davies <thepixeldeveloper@googlemail.com>
2012-04-28 23:16:07 +0100

    Remove STRACE as per Kialls instructions.

--- classes/Kohana/Log/Syslog.php
+++ classes/Kohana/Log/Syslog.php
@@ -42,10 +42,6 @@ class Kohana_Log_Syslog extends Log_Writer {
 	{
 		foreach ($messages as $message)
 		{
-			if (Log::STRACE == $message['level'])
-			{
-				$message['level'] = Log::DEBUG;
-			}
 			syslog($message['level'], $message['body']);
 
 			if (isset($message['additional']['exception']))
