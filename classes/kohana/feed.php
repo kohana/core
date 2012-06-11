ead73aad9ce37f7c8df23324fe7a08f2723fc9d3
chandeeland <chand@chandeeland.org>
2012-06-11 13:13:36 -0400

    improved feeds to handle tag attributes and real atom compat

--- classes/kohana/feed.php
+++ classes/kohana/feed.php
@@ -1,14 +1,159 @@
-<?php defined('SYSPATH') or die('No direct script access.');
+<?php //defined('SYSPATH') or die('No direct script access.');
 /**
  * RSS and Atom feed helper.
  *
  * @package    Kohana
  * @category   Helpers
- * @author     Kohana Team
- * @copyright  (c) 2007-2011 Kohana Team
  * @license    http://kohanaframework.org/license
  */
 class Kohana_Feed {
+	
+	const FEED_FORMAT_ATOM = 'atom';
+	const FEED_FORMAT_RSS2 = 'rss2';
+
+	private $format = self::FEED_FORMAT_RSS2;
+	private $feed = array();
+	private $entry = array();
+	private $encoding = 'UTF-8';
+
+
+	/**
+	 * setter for encoding
+	 */
+	public function set_encoding($encoding)
+	{
+		return $this->encoding = $encoding;
+	}
+
+	/**
+	 * setter for format
+	 */
+	public function set_format($format)
+	{
+		if (in_array($format, array(self::FEED_FORMAT_ATOM, self::FEED_FORMAT_RSS2)))
+		{
+			return $this->format = $format;
+		}
+		throw new Exception('invalid feed format');
+	}
+
+	/**
+	 * add one field to field
+	 *
+	 */ 
+	public function add_feed_info($field, $value, array $attributes = array())
+	{
+		$this->feed[] = array($field, $value, $attributes);
+	}
+
+	/**
+	 * add one field to current entry
+	 *
+	 */
+	public function add_entry_info($field, $value, array $attributes = array())
+	{
+		$this->entry[] = array($field, $value, $attributes);
+	}
+
+
+	/**
+	 * close current entry and open a new one
+	 */
+	public function next_entry()
+	{
+		if (!empty($this->entry))
+		{
+			$this->feed['entry'] = $this->entry;
+			$this->entry = array();
+		}
+	}
+
+	/**
+	 * ouput XML from data collected
+	 *
+	 */
+	public function render() 
+	{	
+		$this->next_entry();
+		switch ($this->format)
+		{
+			case self::FEED_FORMAT_ATOM:
+				$feed = simplexml_load_string('<?xml version="1.0" encoding="'.$encoding.'"?><feed xmlns="http://www.w3.org/2005/Atom"></feed>');
+				$entry_tag = 'entry';
+				break;
+			case self::FEED_FORMAT_RSS2:
+			default:
+				$feed = simplexml_load_string('<?xml version="1.0" encoding="'.$encoding.'"?><rss version="2.0"><channel></channel></rss>');
+				$entry_tag = 'item';
+				break;
+
+		}
+
+		$this->push_fields($this->feed, $feed);
+		foreach ($this->entry as $entry)
+		{
+			$curr_entry = $feed->addChild($entry_tag);
+			$this->push_fields($entry, $curr_entry);
+		}
+
+		// DOM is preferred, as it generates more readable XML
+		if (function_exists('dom_import_simplexml'))
+		{
+			$feed = dom_import_simplexml($feed)->ownerDocument;
+			$feed->formatOutput = TRUE;
+			return $feed->saveXML();
+		}
+	
+		// Export the document as XML
+		return $feed->asXML();
+	}
+
+	/**
+	 * helper which handles pushing sub fields and attributes
+	 *
+	 */
+	protected function push_fields($fields, $sxo)
+	{
+		foreach ($fields as $curr)
+		{
+			list($field, $value, $attributes) = $curr;
+
+			if (is_array($value))
+			{
+				$child = $sxo->addChild($field);
+				foreach ($value as $subfield => $subvalue)
+				{
+					$child->addChild($subfield, $this->scrub_field($subfield, $subvalue));
+				}
+			}
+			else
+			{	
+				$child = $sxo->addChild($field, $this->scrub_field($field, $value));
+			}
+
+			foreach ($attributes as $attribute_name => $attribute_value)
+			{
+				$child->addAttribute($attribute_name, $this->scrub_field($attribute_name, $attribute_value));
+			}
+		}
+	}
+
+
+	/**
+	 * filters field values
+	 *
+	 * currently it tries to detect URL fields and attributes, and set them with current site domain if not already set.
+	 * may be extended to do more.
+	 */
+	protected function scrub_field($field, $value)
+	{
+		if (in_array($field, array('href','url','uri','link'))
+			&& strpos($value, '://') === FALSE)
+		{
+			return URL::site($value, 'http');
+		}
+		return $value;
+	}
 
 	/**
 	 * Parses a remote feed into an array.
@@ -70,107 +215,33 @@ class Kohana_Feed {
 	/**
 	 * Creates a feed from the given parameters.
 	 *
+	 * this function provides backwards compatability
+	 *
 	 * @param   array   feed information
 	 * @param   array   items to add to the feed
-	 * @param   string  define which format to use (only rss2 is supported)
+	 * @param   string  define which format to use (rss2 or atom)
 	 * @param   string  define which encoding to use
 	 * @return  string
 	 */
-	public static function create($info, $items, $format = 'rss2', $encoding = 'UTF-8')
+	public static function create($info, $items, $format = self::FEED_FORMAT_RSS2, $encoding = 'UTF-8')
 	{
 		$info += array('title' => 'Generated Feed', 'link' => '', 'generator' => 'KohanaPHP');
 
-		$feed = '<?xml version="1.0" encoding="'.$encoding.'"?><rss version="2.0"><channel></channel></rss>';
-		$feed = simplexml_load_string($feed);
+		$this->set_encoding($encoding);
+		$this->set_format($format);
 
-		foreach ($info as $name => $value)
-		{
-			if ($name === 'image')
-			{
-				// Create an image element
-				$image = $feed->channel->addChild('image');
-
-				if ( ! isset($value['link'], $value['url'], $value['title']))
-				{
-					throw new Kohana_Exception('Feed images require a link, url, and title');
-				}
-
-				if (strpos($value['link'], '://') === FALSE)
-				{
-					// Convert URIs to URLs
-					$value['link'] = URL::site($value['link'], 'http');
-				}
-
-				if (strpos($value['url'], '://') === FALSE)
-				{
-					// Convert URIs to URLs
-					$value['url'] = URL::site($value['url'], 'http');
-				}
-
-				// Create the image elements
-				$image->addChild('link', $value['link']);
-				$image->addChild('url', $value['url']);
-				$image->addChild('title', $value['title']);
-			}
-			else
-			{
-				if (($name === 'pubDate' OR $name === 'lastBuildDate') AND (is_int($value) OR ctype_digit($value)))
-				{
-					// Convert timestamps to RFC 822 formatted dates
-					$value = date('r', $value);
-				}
-				elseif (($name === 'link' OR $name === 'docs') AND strpos($value, '://') === FALSE)
-				{
-					// Convert URIs to URLs
-					$value = URL::site($value, 'http');
-				}
-
-				// Add the info to the channel
-				$feed->channel->addChild($name, $value);
-			}
+		foreach ($info as $field => $value)
+		{	
+			$this->set_feed_info($field, $value, array());
 		}
 
 		foreach ($items as $item)
 		{
-			// Add the item to the channel
-			$row = $feed->channel->addChild('item');
-
-			foreach ($item as $name => $value)
-			{
-				if ($name === 'pubDate' AND (is_int($value) OR ctype_digit($value)))
-				{
-					// Convert timestamps to RFC 822 formatted dates
-					$value = date('r', $value);
-				}
-				elseif (($name === 'link' OR $name === 'guid') AND strpos($value, '://') === FALSE)
-				{
-					// Convert URIs to URLs
-					$value = URL::site($value, 'http');
-				}
-
-				// Add the info to the row
-				$row->addChild($name, $value);
-			}
+			$this->next_entry();
+			foreach ($item as $field => $value) $this->set_entry_info($field, $value, array());
 		}
 
-		if (function_exists('dom_import_simplexml'))
-		{
-			// Convert the feed object to a DOM object
-			$feed = dom_import_simplexml($feed)->ownerDocument;
-
-			// DOM generates more readable XML
-			$feed->formatOutput = TRUE;
-
-			// Export the document as XML
-			$feed = $feed->saveXML();
-		}
-		else
-		{
-			// Export the document as XML
-			$feed = $feed->asXML();
-		}
-
-		return $feed;
+		return $this->render();
 	}
 
 } // End Feed
