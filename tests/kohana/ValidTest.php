kohana/core

commit 23e1b0bc03f5457aa54a4a2b95a50b4c0313979a

Added $step parameter to Valid::range(), fixes #4043

tests/kohana/ValidTest.php
@@ -848,16 +848,19 @@ class Kohana_ValidTest extends Unittest_TestCase
 	public function provider_range()
 	{
 		return array(
-			array(1,  0,  2, TRUE),
-			array(-1, -5, 0, TRUE),
-			array(-1, 0,  1, FALSE),
-			array(1,  0,  0, FALSE),
-			array(2147483647, 0, 200000000000000, TRUE),
-			array(-2147483647, -2147483655, 2147483645, TRUE),
+			array(1,  0,  2, NULL, TRUE),
+			array(-1, -5, 0, NULL, TRUE),
+			array(-1, 0,  1, NULL, FALSE),
+			array(1,  0,  0, NULL, FALSE),
+			array(2147483647, 0, 200000000000000, NULL, TRUE),
+			array(-2147483647, -2147483655, 2147483645, NULL, TRUE),
+			// #4043
+			array(2, 0, 10, 2, TRUE),
+			array(3, 0, 10, 2, FALSE),
 			// Empty test
-			array('', 5, 10, FALSE),
-			array(NULL, 5, 10, FALSE),
-			array(FALSE, 5, 10, FALSE),
+			array('', 5, 10, NULL, FALSE),
+			array(NULL, 5, 10, NULL, FALSE),
+			array(FALSE, 5, 10, NULL, FALSE),
 		);
 	}
 
@@ -873,11 +876,11 @@ class Kohana_ValidTest extends Unittest_TestCase
 	 * @param integer $max       Upper bound
 	 * @param boolean $expected  Is Number within the bounds of $min && $max
 	 */
-	public function test_range($number, $min, $max, $expected)
+	public function test_range($number, $min, $max, $step, $expected)
 	{
 		$this->AssertSame(
 			$expected,
-			Valid::range($number, $min, $max)
+			Valid::range($number, $min, $max, $step)
 		);
 	}
 

classes/kohana/valid.php
@@ -473,11 +473,25 @@ class Kohana_Valid {
 	 * @param   string  $number number to check
 	 * @param   integer $min    minimum value
 	 * @param   integer $max    maximum value
+	 * @param   integer $step   increment size
 	 * @return  boolean
 	 */
-	public static function range($number, $min, $max)
+	public static function range($number, $min, $max, $step = NULL)
 	{
-		return ($number >= $min AND $number <= $max);
+		if ($number <= $min OR $number >= $max)
+		{
+			// Number is outside of range
+			return FALSE;
+		}
+
+		if ( ! $step)
+		{
+			// Default to steps of 1
+			$step = 1;
+		}
+
+		// Check step requirements
+		return (($number - $min) % $step === 0);
 	}
 
 	/**

commit 4b635e5bf07ef4d78ab5dc3172b8d4783aa071a1

Fix Valid::range bug excluding min/max values
Closes #4672 bug introduced in 23e1b0b

classes/Kohana/Valid.php
@@ -478,7 +478,7 @@ class Kohana_Valid {
 	 */
 	public static function range($number, $min, $max, $step = NULL)
 	{
-		if ($number <= $min OR $number >= $max)
+		if ($number < $min OR $number > $max)
 		{
 			// Number is outside of range
 			return FALSE;

tests/kohana/ValidTest.php
@@ -858,6 +858,15 @@ class Kohana_ValidTest extends Unittest_TestCase
 			// #4043
 			array(2, 0, 10, 2, TRUE),
 			array(3, 0, 10, 2, FALSE),
+			// #4672
+			array(0, 0, 10, NULL, TRUE),
+			array(10, 0, 10, NULL, TRUE),
+			array(-10, -10, 10, NULL, TRUE),
+			array(-10, -1, 1, NULL, FALSE),
+			array(0, 0, 10, 2, TRUE), // with $step
+			array(10, 0, 10, 2, TRUE),
+			array(10, 0, 10, 3, FALSE), // max outside $step
+			array(12, 0, 12, 3, TRUE),
 			// Empty test
 			array('', 5, 10, NULL, FALSE),
 			array(NULL, 5, 10, NULL, FALSE),