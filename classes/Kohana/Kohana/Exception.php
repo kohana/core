bb1d177e864552d5fc2254c0a7fb08f0ff5946a0
Bharat Mediratta <bharat@menalto.com>
2013-05-01 21:23:57 -0400

    Fix security preamble and make it consistent in a few cases where it deviates slightly.

--- classes/Kohana/Kohana/Exception.php
+++ classes/Kohana/Kohana/Exception.php
@@ -1,4 +1,4 @@
-<?php defined('SYSPATH') OR die('No direct access');
+<?php defined('SYSPATH') OR die('No direct script access.');
 /**
  * Kohana exception class. Translates exceptions using the [I18n] class.
  *
