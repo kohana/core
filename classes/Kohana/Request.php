e10fcb991800525c483fde93d1e1f26740cca670
kostyatretyak <ktretyakgm@gmail.com>
2013-12-20 15:49:48 +0200

    Fixed link on Request::instance()

--- classes/Kohana/Request.php
+++ classes/Kohana/Request.php
@@ -38,7 +38,7 @@ class Kohana_Request implements HTTP_Request {
 
 	/**
 	 * Creates a new request object for the given URI. New requests should be
-	 * created using the [Request::instance] or [Request::factory] methods.
+	 * created using this methods.
 	 *
 	 *     $request = Request::factory($uri);
 	 *
@@ -631,7 +631,7 @@ class Kohana_Request implements HTTP_Request {
 
 	/**
 	 * Creates a new request object for the given URI. New requests should be
-	 * created using the [Request::instance] or [Request::factory] methods.
+	 * created using the [Request::factory] methods.
 	 *
 	 *     $request = new Request($uri);
 	 *
