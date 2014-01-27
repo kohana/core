34f05c23cdee8b12f02859940552561e77c7e596
peppelauro <peppe.lauro@gmail.com>
2014-01-27 23:35:47 +0100

    Create it.php
    Created it i18n, with translations of validate messages.

---
+++ i18n/it.php
@@ -0,0 +1,30 @@
+ <?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+
+ ':field must contain only letters'                        => ':field deve contenere solo lettere',
+ ':field must contain only numbers, letters and dashes'	   => ':field deve contenere solo numeri, lettere e -',
+ ':field must contain only letters and numbers'	           => ':field deve contenere solo lettere e numeri',
+ ':field must be a color'	                               => ':field deve essere un colore',
+ ':field must be a credit card number'                     => ':field deve essere un numero di carta di credito',
+ ':field must be a date'                                   => ':field deve essere una data',
+ ':field must be a decimal with :param2 places'            => ':field deve essere un numero con :param2 decimali',
+ ':field must be a digit'                                  => ':field deve essere un numero',
+ ':field must be an email address'                         => ':field deve essere un indirizzo email',
+ ':field must contain a valid email domain'	               => ':field deve avere un dominio email valido',
+ ':field must equal :param2'	                           => ':field deve essere uguale a :param2',
+ ':field must be exactly :param2 characters long'	       => ':field deve essere lungo esattamente :param2 caratteri',
+ ':field must be one of the available options'	           => ':field deve essere una delle opzioni disponibili',
+ ':field must be an ip address'	                           => ':field deve essere un indirizzo ip',
+ ':field must be the same as :param3'                      => ':field deve essere lo stesso di :param3',
+ ':field must be at least :param2 characters long'         => ':field deve essere lungo almeno :param2',
+ ':field must not exceed :param2 characters long'          => ':field non deve superare i :param2 caratteri',
+ ':field must not be empty'                                => ':field non può essere vuoto',
+ ':field must be numeric'                                  => ':field deve essere numerico',
+ ':field must be a phone number'                           => ':field deve essere un numero di telefono',
+ ':field must be within the range of :param2 to :param3'   => ':field deve essere compreso tra :param2 e :param3',
+ ':field does not match the required format'               => ':field non rispetta il formato richiesto',
+ ':field must be a url'                                    => ':field deve essere un url',
+	
+
+);
