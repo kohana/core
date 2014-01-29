c17a98407dc4ea653dbcc7160725f62bf850a66e
Rob Taylor <robt@hoopassociates.co.uk>
2014-01-29 16:34:09 +0000

    Fixed typo in comments for Kohana_View::__toString()

--- classes/Kohana/View.php
+++ classes/Kohana/View.php
@@ -232,7 +232,7 @@ class Kohana_View {
 			/**
 			 * Display the exception message.
 			 *
-			 * We use this method here because it's impossible to throw and
+			 * We use this method here because it's impossible to throw an
 			 * exception from __toString().
 			 */
 			$error_response = Kohana_Exception::_handler($e);
