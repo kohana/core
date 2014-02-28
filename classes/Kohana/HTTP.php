c2227827ab4c994f7b62dc8781df6bf6b9c9687d
Soon Van <cog@randomecho.com>
2014-02-28 13:28:53 -0500

    Update license URLs in PHPDoc - Ref: #4821
    Found links pointing to old location
    of license instead of where they are
    now at http://kohanaframework.org/license

--- classes/Kohana/HTTP.php
+++ classes/Kohana/HTTP.php
@@ -11,8 +11,8 @@
  * @category   HTTP
  * @author     Kohana Team
  * @since      3.1.0
- * @copyright  (c) 2008-2012 Kohana Team
- * @license    http://kohanaphp.com/license
+ * @copyright  (c) 2008-2014 Kohana Team
+ * @license    http://kohanaframework.org/license
  */
 abstract class Kohana_HTTP {
 
