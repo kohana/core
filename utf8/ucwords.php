611d56989103789944ab9aa91904a5fdc7a40ecb
Kemo <kemal.delalic@gmail.com>
2014-04-17 01:45:34 +0200

    Use `preg_replace_callback()` in `_ucwords()`
    /e modifier is deprecated since PHP 5.4

--- utf8/ucwords.php
+++ utf8/ucwords.php
@@ -15,9 +15,10 @@ function _ucwords($str)
 
 	// [\x0c\x09\x0b\x0a\x0d\x20] matches form feeds, horizontal tabs, vertical tabs, linefeeds and carriage returns.
 	// This corresponds to the definition of a 'word' defined at http://php.net/ucwords
-	return preg_replace(
-		'/(?<=^|[\x0c\x09\x0b\x0a\x0d\x20])[^\x0c\x09\x0b\x0a\x0d\x20]/ue',
-		'UTF8::strtoupper(\'$0\')',
-		$str
-	);
+	return preg_replace_callback(
+		'/(?<=^|[\x0c\x09\x0b\x0a\x0d\x20])[^\x0c\x09\x0b\x0a\x0d\x20]/u', 
+		function($matches){
+			return UTF8::strtoupper($matches[0]);
+		},
+		$str);
 }
