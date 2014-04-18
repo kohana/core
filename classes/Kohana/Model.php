80039cbe218f673329990bea21ad6545f49a3cdc
Anton <WinterSilence@users.noreply.github.com>
2014-04-18 11:24:18 -0700

    Flexible model
    Attribute $settings: No need override factory method in ORM and etc.
    
    https://github.com/kohana/orm/blob/3.4/master/classes/Kohana/ORM.php#L41

--- classes/Kohana/Model.php
+++ classes/Kohana/Model.php
@@ -1,29 +1,34 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 /**
- * Model base class. All models should extend this class.
+ * Abstract model class. All models should extend this class.
+ * See [MVC models](kohana/mvc/models) for more information.
  *
  * @package    Kohana
- * @category   Models
+ * @category   Model
  * @author     Kohana Team
- * @copyright  (c) 2008-2012 Kohana Team
+ * @copyright  (c) 2008-2014 Kohana Team
  * @license    http://kohanaframework.org/license
  */
 abstract class Kohana_Model {
+
+	// Prefix to class name
+	const CLASS_PREFIX = 'Model_';
 
 	/**
 	 * Create a new model instance.
 	 *
 	 *     $model = Model::factory($name);
 	 *
-	 * @param   string  $name   model name
+	 * @param   string  $name      Model name
+	 * @param   mixed   $settings  Model settings, eg: id
 	 * @return  Model
 	 */
-	public static function factory($name)
+	public static function factory($name, $settings = NULL)
 	{
 		// Add the model prefix
-		$class = 'Model_'.$name;
+		$class = Model::CLASS_PREFIX.$name;
 
-		return new $class;
+		return new $class($settings);
 	}
 
 }
