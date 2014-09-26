7bbc0d2067df64ae550e465567bf0c52b8a0402b
Andrew Coulton <andrew@ingenerator.com>
2014-09-26 11:40:52 +0100

    Force Debug::dump marker to alpha for type safety [Fixes #559]
    Resolves a flickering test (and flickering Debug::dump output)
    where internal recursion markers are left in dumped arrays
    if the current system microtime produces a uniqid with only
    numeric characters.

--- classes/Kohana/Debug.php
+++ classes/Kohana/Debug.php
@@ -133,8 +133,8 @@ class Kohana_Debug {
 
 			if ($marker === NULL)
 			{
-				// Make a unique marker
-				$marker = uniqid("\x00");
+				// Make a unique marker - force it to be alphanumeric so that it is always treated as a string array key
+				$marker = uniqid("\x00")."x";
 			}
 
 			if (empty($var))
