eb55e8df2083a279917bd9030a6a6dd190880c13
Andrew Coulton <andrew@ingenerator.com>
2014-10-10 16:19:12 +0100

    Update travis configuration and composer dependencies for 3.4 series

--- koharness.php
+++ koharness.php
@@ -1,6 +1,7 @@
 <?php
 // Configuration for koharness - builds a standalone skeleton Kohana app for running unit tests
 return array(
+	'kohana_version' => '3.4',
 	'modules' => array(
 		'unittest' => __DIR__ . '/vendor/kohana/unittest'
 	),
