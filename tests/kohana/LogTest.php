2a4a27b18c0140dc17ca4994b9708efb70a3cc1d
Samuel Demirdjian <sam@enov.ws>
2015-04-17 14:08:56 +0300

    add tests for generic log() and 8 level methods
    Tests are failing for the time being because
    the trace information within the log adds
    an additional entry in case of specialized
    `debug()`, `info()`... method calls vs
    the generic `log()` calls

--- tests/kohana/LogTest.php
+++ tests/kohana/LogTest.php
@@ -107,5 +107,104 @@ class Kohana_LogTest extends Unittest_TestCase
 		$this->assertAttributeSame(array(), '_writers', $logger);
 	}
 
+	public function provider_logging()
+	{
+		return [
+			[
+				'emergency',
+				['emergency', Log::EMERGENCY, \Psr\Log\LogLevel::EMERGENCY,],
+				'In case of emergency break glass.',
+			],
+			[
+				'alert',
+				['alert', Log::ALERT, \Psr\Log\LogLevel::ALERT,],
+				'Alert: this is not a dialog box.',
+			],
+			[
+				'critical',
+				['critical', Log::CRITICAL, \Psr\Log\LogLevel::CRITICAL,],
+				'Eye strain reaching critical levels!',
+			],
+			[
+				'error',
+				['error', Log::ERROR, \Psr\Log\LogLevel::ERROR,],
+				'You must be doing something wrong.',
+			],
+			[
+				'warning',
+				['warning', Log::WARNING, \Psr\Log\LogLevel::WARNING,],
+				'Too much cafeine blows up your brain.',
+			],
+			[
+				'notice',
+				['notice', Log::NOTICE, \Psr\Log\LogLevel::NOTICE,],
+				'Not funny any more.',
+			],
+			[
+				'info',
+				['info', Log::INFO, \Psr\Log\LogLevel::INFO,],
+				'Looking good today.',
+			],
+			[
+				'debug',
+				['debug', Log::DEBUG, \Psr\Log\LogLevel::DEBUG,],
+				'Never again.',
+			],
+		];
+	}
+
+	/**
+	 * Tests logging calls with a stub Log_Writer
+	 *
+	 * @test
+	 * @dataProvider provider_logging
+	 */
+	public function test_logging($method, array $levels, $message)
+	{
+		// initialize
+		$logger = new Log;
+		$writer = new Kohana_LogTest_Log_Writer_Memory();
+		$logger->attach($writer);
+
+		// test logging with specialized method
+		$logger->$method($message);
+		$logger->write();
+		$expected = $writer->logs[0];
+		// unset the first trace to the specialized method
+		array_shift($expected['trace']);
+
+		// reset writer's logs
+		$writer->logs = array();
+
+		// test logging with the generic log method
+		foreach ($levels as $level) {
+			$method = 'log';
+			$logger->$method($level, $message);
+		}
+		$logger->write();
+
+		// assertions
+		foreach ($writer->logs as $log) {
+			$this->assertSame($expected, $log);
+		}
+	}
+
+}
+
+/**
+ * A Log_Writer that appends logs to an internal array, used for testing
+ */
+class Kohana_LogTest_Log_Writer_Memory extends Log_Writer {
+
+	public $logs = array();
+
+	/**
+	 *
+	 * @param array $logs
+	 */
+	public function write(array $logs)
+	{
+		$this->logs = array_merge($this->logs, $logs);
+	}
 
 }
