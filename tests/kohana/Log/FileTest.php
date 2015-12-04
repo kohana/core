df30f2fb7b95f31b4bddc182cdb6408c56c4eee3
Samuel Demirdjian <sam@enov.ws>
2015-12-04 10:01:54 +0200

    Test writer does not create new log file if exists, only appends

--- tests/kohana/Log/FileTest.php
+++ tests/kohana/Log/FileTest.php
@@ -105,6 +105,68 @@ LOG
 	}
 
 	/**
+	 * Provider for test_writer_does_not_create_existing_file_but_only_appends
+	 *
+	 */
+	public function provider_writer_does_not_create_existing_file_but_only_appends()
+	{
+		return [
+			// data set #0
+			[
+				// raw log messages
+				[
+					[
+						'time' => strtotime('2015-11-19 10:05:48'),
+						'level' => \Psr\Log\LogLevel::DEBUG,
+						'body' => 'dummy text',
+						'line' => 10,
+						'file' => '/path/to/file.php',
+					]
+				],
+				// existing file content
+<<<'EXISTING'
+<?php exit; ?>
+
+2015-11-19 09:01:30 --- INFO: dummy text that already existed in /path/to/some/file.php:10
+
+EXISTING
+				,
+				// expected file contents
+<<<'EXPECTED'
+<?php exit; ?>
+
+2015-11-19 09:01:30 --- INFO: dummy text that already existed in /path/to/some/file.php:10
+2015-11-19 10:05:48 --- DEBUG: dummy text in /path/to/file.php:10
+
+EXPECTED
+
+			],
+		];
+	}
+
+	/**
+	 * Tests that writer does not create a new log file, when one exists, but
+	 * only appends to it
+	 *
+	 * @test
+	 * @dataProvider provider_writer_does_not_create_existing_file_but_only_appends
+	 */
+	public function test_writer_does_not_create_existing_file_but_only_appends(array $log_entries, $existing_file_contents, $expected_file_contents)
+	{
+		$writer = new Kohana_Log_FileTest_Testable_Log_File($this->vfs_root->url());
+
+		// prepare environment: create log file with some contents
+		mkdir($writer->get_directory(), 0777, TRUE);
+		file_put_contents($writer->get_directory() . $writer->get_filename(), $existing_file_contents);
+
+		// write some logs, writer should only append to existing log file
+		$writer->write($log_entries);
+
+		// assert
+		$this->assertSame($expected_file_contents, $writer->get_written_logs());
+	}
+
+	/**
 	 * Data provider for test_filter
 	 *
 	 * @return array
@@ -195,15 +257,23 @@ LOG
  */
 class Kohana_Log_FileTest_Testable_Log_File extends Log_File {
 
-	public function get_written_logs()
+	public function get_directory()
 	{
-		$file = $this->_directory
+		return $this->_directory
 		  . date('Y')
 		  . DIRECTORY_SEPARATOR
 		  . date('m')
-		  . DIRECTORY_SEPARATOR
-		  . date('d').EXT;
-		return file_get_contents($file);
+		  . DIRECTORY_SEPARATOR;
+	}
+	
+	public function get_filename()
+	{
+		return date('d') . EXT;
+	}
+
+	public function get_written_logs()
+	{
+		return file_get_contents($this->get_directory() . $this->get_filename());
 	}
 
 }
