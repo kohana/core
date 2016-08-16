f3cd2ef14871cc6b8c9b9c6f32ce52ab447a3efb
Samuel Demirdjian <sam@enov.ws>
2016-08-16 08:36:11 +0300

    Remove deprecated Security::strip_image_tags method
    Related to Github issue https://github.com/kohana/kohana/issues/107

--- tests/Kohana/SecurityTest.php
+++ tests/Kohana/SecurityTest.php
@@ -37,30 +37,6 @@ class Kohana_SecurityTest extends Unittest_TestCase
 	}
 
 	/**
-	 * Provides test data for test_strip_image_tags()
-	 *
-	 * @return array Test data sets
-	 */
-	public function provider_strip_image_tags()
-	{
-		return array(
-			array('foo', '<img src="foo" />'),
-		);
-	}
-
-	/**
-	 * Tests Security::strip_image_tags()
-	 *
-	 * @test
-	 * @dataProvider provider_strip_image_tags
-	 * @covers Security::strip_image_tags
-	 */
-	public function test_strip_image_tags($expected, $input)
-	{
-		$this->assertSame($expected, Security::strip_image_tags($input));
-	}
-
-	/**
 	 * Provides test data for Security::token()
 	 *
 	 * @return array Test data sets
